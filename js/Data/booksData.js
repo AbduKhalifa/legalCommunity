706f58e875e621c0d6fc4f2d911f9d76cb0b0e18
Poda <112504016+AbduKhalifa@users.noreply.github.com>
2023-10-01 20:35:52 +0300

    Update booksData.js

--- js/Data/booksData.js
+++ js/Data/booksData.js
@@ -15,37 +15,37 @@ const BOOKS_3 = [
     {
         "title":"قانون العمل الجزء الاول",
         "explain":"إضغظ علي اسم الكتاب للتحميل ",
-        "path" : "/books/y3/Work_Law_Part_1.pdf"
+        "path" : "./books/y3/Work_Law_Part_1.pdf"
     },
     {
         "title":"قانون العمل الجزء الثاني",
         "explain":"إضغظ علي اسم الكتاب للتحميل ",
-        "path" : "/books/y3/Work_Law_Part_2.pdf"
+        "path" : "./books/y3/Work_Law_Part_2.pdf"
     },
     {
         "title":"جنائي قسم خاص",
         "explain":"إضغظ علي اسم الكتاب للتحميل ",
-        "path" : "/books/y3/Special_Criminal_Department.pdf"
+        "path" : "./books/y3/Special_Criminal_Department.pdf"
     },
     {
         "title":"القانون التجاري",
         "explain":"إضغظ علي اسم الكتاب للتحميل ",
-        "path" : "/books/y3/Commercial_Law.pdf"
+        "path" : "./books/y3/Commercial_Law.pdf"
     },
     {
         "title":"العقود المدنيه",
         "explain":"إضغظ علي اسم الكتاب للتحميل ",
-        "path" : "/books/y3/Civil_Contracts.pdf"
+        "path" : "./books/y3/Civil_Contracts.pdf"
     },
     {
         "title":"القضاء الإداري",
         "explain":"إضغظ علي اسم الكتاب للتحميل ",
-        "path" : "/books/y3/Administrative_Judiciary.pdf"
+        "path" : "./books/y3/Administrative_Judiciary.pdf"
     },
     {
         "title":"تشريعات حمايه البيئه",
         "explain":"إضغظ علي اسم الكتاب للتحميل ",
-        "path" : "/books/y3/Environmental_Protection_Legislation.pdf"
+        "path" : "./books/y3/Environmental_Protection_Legislation.pdf"
     }
 ];
 
